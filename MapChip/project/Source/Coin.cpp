79830a1072e190529e890846530edbcc96870edd
花井直人 <nao@hanamaru.games>
2024-10-24 17:30:46 +0900

    SwordBout追加

--- MapChip/project/Source/Coin.cpp
+++ MapChip/project/Source/Coin.cpp
@@ -18,22 +18,33 @@ Coin::~Coin()
 
 void Coin::Update()
 {
-	if (got) {
-		// プレイヤーに吸い込む
-
-		return;
-	}
-
 	Player* pl = FindGameObject<Player>();
 	if (pl == nullptr) {
 		return;
 	}
+	if (got) {
+		// プレイヤーに吸い込む
+		// ①自分（Coin）からプレイヤーへのベクトルを作る
+		VECTOR v = pl->Position() - position;
+		// ②、①のベクトルの長さを、吸い込みの速度(5.0f)にする
+		VECTOR v2 = VNorm(v) * gotSpeed;
+		// ③、②を自分の座標に加える
+		position = position + v2;
+		// ④自分が、プレイヤーに近づいたら(40.0f以内)、DestroyMe()する
+		if (VSize(v) - gotSpeed < 40.0f) {
+			DestroyMe();
+		}
+		gotSpeed += 0.5f;
+		return;
+	}
+
 	VECTOR plPos = pl->Position(); // プレイヤーの座標
 	// 自分の座標はposition
 	VECTOR diff = plPos - position;
 	float len = VSize(diff);
-	if (len < 100.0f) {
+	if (len < 200.0f) {
 		got = true;
+		gotSpeed = 3.0f;
 	}
 }
 
--- MapChip/project/Source/Coin.h
+++ MapChip/project/Source/Coin.h
@@ -13,4 +13,5 @@ private:
 	VECTOR position;
 	int hModel;
 	bool got;
+	float gotSpeed;
 };
